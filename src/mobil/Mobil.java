707c6d101194e0b549b9d2f469e9068f69c28b70
rinafahridawati <rinafahridawati@yahoo.com>
2018-03-24 14:20:56 +0800

    Membuat Atribut dan Behavior

--- src/mobil/Mobil.java
+++ src/mobil/Mobil.java
@@ -9,7 +9,31 @@ package mobil;
  * @author rina
  */
 public class Mobil {
+String Pemilik, Merek, Warna, NoPolisi, TahunProduksi;
+String Pemilik1, Merek1, Warna1, NoPolisi1, TahunProduksi1;
+String Pemilik2, Merek2, Warna2, NoPolisi2, TahunProduksi2;
 
+void cetak () {
+       System.out.println ("==========================");
+       System.out.println("Nama Pemilik Mobil :"+Pemilik);
+       System.out.println("Merek Mobil :"+Merek);
+       System.out.println("Warna Mobil :"+Warna);
+       System.out.println("Nomor Polisi Mobil :"+NoPolisi);
+       System.out.println("Tahun Produksi Mobil :"+TahunProduksi);
+       System.out.println ("==========================");
+       System.out.println("Nama Pemilik Mobil :"+Pemilik1);
+       System.out.println("Merek Mobil :"+Merek1);
+       System.out.println("Warna Mobil :"+Warna1);
+       System.out.println("Nomor Polisi Mobil :"+NoPolisi1);
+       System.out.println("Tahun Produksi Mobil :"+TahunProduksi1);
+       System.out.println ("==========================");
+       System.out.println("Nama Pemilik Mobil :"+Pemilik2);
+       System.out.println("Merek Mobil :"+Merek2);
+       System.out.println("Warna Mobil :"+Warna2);
+       System.out.println("Nomor Polisi Mobil :"+NoPolisi2);
+       System.out.println("Tahun Produksi Mobil :"+TahunProduksi2);
+
+}
     /**
      * @param args the command line arguments
      */
