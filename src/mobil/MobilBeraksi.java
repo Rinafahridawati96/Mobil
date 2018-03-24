566b4c7e49a456bc60b337fc9fe5111c40d21b5c
rinafahridawati <rinafahridawati@yahoo.com>
2018-03-24 14:28:07 +0800

    Membuat Class Aksi dan Method Main

--- src/mobil/MobilBeraksi.java
+++ src/mobil/MobilBeraksi.java
@@ -9,5 +9,29 @@ package mobil;
  * @author rina
  */
 public class MobilBeraksi {
-    
+    public static void main(String[] args) {
+       Mobil Mobilku = new Mobil ();
+       
+       
+       Mobilku.Pemilik = "Rina Fahridawati";
+       Mobilku.Merek = "BMW";
+       Mobilku.Warna = "Merah Hitam":
+       Mobilku.NoPolisi = "DA 1234 PH":
+       Mobilku.TahunProduksi = "2010":
+               
+       
+       Mobilku.Pemilik1 = "Kishan";
+       Mobilku.Merek1 = "Lambo";
+       Mobilku.Warna1 = "Merah Kuning":
+       Mobilku.NoPolisi1 = "DA 2341 PH":
+       Mobilku.TahunProduksi1 = "2013":
+               
+                          
+       Mobilku.Pemilik2 = "Alfin";
+       Mobilku.Merek2 = "";
+       Mobilku.Warna2 = "Biru Hijau":
+       Mobilku.NoPolisi2 = "DA 4321 PH":
+       Mobilku.TahunProduksi2 = "2016":
+    }
+ 
 }
