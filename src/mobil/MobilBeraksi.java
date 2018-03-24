a5301fed6f494d745cbe1189ebf776963b3367f7
rinafahridawati <rinafahridawati@yahoo.com>
2018-03-24 14:30:09 +0800

    Menggil Method

--- src/mobil/MobilBeraksi.java
+++ src/mobil/MobilBeraksi.java
@@ -15,23 +15,25 @@ public class MobilBeraksi {
        
        Mobilku.Pemilik = "Rina Fahridawati";
        Mobilku.Merek = "BMW";
-       Mobilku.Warna = "Merah Hitam":
-       Mobilku.NoPolisi = "DA 1234 PH":
-       Mobilku.TahunProduksi = "2010":
+       Mobilku.Warna = "Merah Hitam";
+       Mobilku.NoPolisi = "DA 1234 PH";
+       Mobilku.TahunProduksi = "2010";
                
        
        Mobilku.Pemilik1 = "Kishan";
        Mobilku.Merek1 = "Lambo";
-       Mobilku.Warna1 = "Merah Kuning":
-       Mobilku.NoPolisi1 = "DA 2341 PH":
-       Mobilku.TahunProduksi1 = "2013":
+       Mobilku.Warna1 = "Merah Kuning";
+       Mobilku.NoPolisi1 = "DA 2341 PH";
+       Mobilku.TahunProduksi1 = "2013";
                
                           
        Mobilku.Pemilik2 = "Alfin";
        Mobilku.Merek2 = "";
-       Mobilku.Warna2 = "Biru Hijau":
-       Mobilku.NoPolisi2 = "DA 4321 PH":
-       Mobilku.TahunProduksi2 = "2016":
+       Mobilku.Warna2 = "Biru Hijau";
+       Mobilku.NoPolisi2 = "DA 4321 PH";
+       Mobilku.TahunProduksi2 = "2016";
+       
+       Mobilku.cetak();
     }
  
 }
